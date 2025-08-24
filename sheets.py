49a9864291cb4c7874ffee55ea587dad788ad2af
Debolina Chakraborty <d.chakraborty2727@gmail.com>
2025-08-24 13:31:00 +0530

    Add files via upload

--- sheets.py
+++ sheets.py
@@ -23,3 +23,24 @@ def test_api():
 
 if __name__ == '__main__':
     test_api()
+
+
+import pygsheets
+
+# Authorize using the JSON file
+gc = pygsheets.authorize(service_file='credentials_sheets.json')
+
+# Open a Google Sheet by title
+sh = gc.open_by_key('1XPTtr9T2g5MxhYqu61A5lOi9JdWjYW4-ItvJIhfLTYI')
+
+# Select the first sheet
+wks = sh[0]
+
+# Update a cell
+wks.update_value('A1', 'Hello from pygsheets!')
+
+# Read the value back from cell A1
+value = wks.get_value('A1')
+
+# Print the value
+print('Value in A1:', value)
